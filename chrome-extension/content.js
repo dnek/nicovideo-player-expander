6ca8680500d13e7e5077a9b7fd8d59fcf85c4749
DNEK <dnek.app@gmail.com>
2024-10-29 17:41:10 +0900

    replace css media query fullscreen with fullscreen api to ignore browser app fullscreen

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -36,61 +36,59 @@ const GM_addStyle = (content) => {
     };
 
     const theaterModeStyleEl = GM_addStyle(`
-@media not all and (display-mode: fullscreen) {
-    #root > div.min-w_\\[max-content\\] {
-        min-width: auto;
+#root > div.min-w_\\[max-content\\] {
+    min-width: auto;
+}
+div[aria-label="nicovideo-content"] > section.grid-template-areas_\\[_\\"player_sidebar\\"_\\"meta_sidebar\\"_\\"bottom_sidebar\\"_\\"\\._sidebar\\"_\\] {
+    --watch-player-expandable-width-by-browser: calc(
+        100vw
+        - var(--watch-layout-gap-width) * 2
+        - var(--scrollbar-width)
+    );
+    --watch-player-expandable-height-by-browser: calc(
+        100vh
+        - var(--common-header-height)
+        - var(--web-header-height)
+        - var(--watch-controller-height)
+        - var(--watch-actionbar-height)
+        - var(--watch-player-bottom-margin-height)
+    );
+    --watch-player-expandable-width: calc(
+        min(
+            var(--watch-player-expandable-width-by-browser),
+            var(--watch-player-expandable-height-by-browser) * (16 / 9)
+        )
+    );
+    grid-template-columns: var(--watch-player-expandable-width);
+    grid-template-areas: "player" "meta" "bottom" "sidebar";
+}
+div.grid-area_\\[sidebar\\] > div[data-nvpc-part="floating"] > section {
+    position: fixed;
+    top: calc(var(--sizes-common-header-in-view-height) + var(--sizes-web-header-height) + var(--spacing-x3));
+    width: var(--sizes-watch-sidebar-width);
+}
+div:has(> div > button[aria-label="コメント投稿ボタン"]) {
+    min-width: auto !important;
+}
+@media (max-width: 720px), (max-height: 720px) {
+    div:has(> button[aria-label="設定"]) {
+        gap: 0;
     }
-    div[aria-label="nicovideo-content"] > section.grid-template-areas_\\[_\\"player_sidebar\\"_\\"meta_sidebar\\"_\\"bottom_sidebar\\"_\\"\\._sidebar\\"_\\] {
-        --watch-player-expandable-width-by-browser: calc(
-            100vw
-            - var(--watch-layout-gap-width) * 2
-            - var(--scrollbar-width)
-        );
-        --watch-player-expandable-height-by-browser: calc(
-            100vh
-            - var(--common-header-height)
-            - var(--web-header-height)
-            - var(--watch-controller-height)
-            - var(--watch-actionbar-height)
-            - var(--watch-player-bottom-margin-height)
-        );
-        --watch-player-expandable-width: calc(
-            min(
-                var(--watch-player-expandable-width-by-browser),
-                var(--watch-player-expandable-height-by-browser) * (16 / 9)
-            )
-        );
-        grid-template-columns: var(--watch-player-expandable-width);
-        grid-template-areas: "player" "meta" "bottom" "sidebar";
-    }
-    div.grid-area_\\[sidebar\\] > div[data-nvpc-part="floating"] > section {
-        position: fixed;
-        top: calc(var(--sizes-common-header-in-view-height) + var(--sizes-web-header-height) + var(--spacing-x3));
-        width: var(--sizes-watch-sidebar-width);
-    }
-    div:has(> div > button[aria-label="コメント投稿ボタン"]) {
-        min-width: auto !important;
-    }
-    @media (max-width: 720px), (max-height: 720px) {
-        div:has(> button[aria-label="設定"]) {
-            gap: 0;
-        }
-    }
-    @media (max-width: 650px), (max-height: 680px) {
-        div.d_flex:has(> button[aria-label$=" 秒戻る"]) {
-            gap: 0;
-            > div.d_flex {
-                flex-flow: column;
-                > span.mx_x0_5 {
-                    display: none;
-                }
+}
+@media (max-width: 650px), (max-height: 680px) {
+    div.d_flex:has(> button[aria-label$=" 秒戻る"]) {
+        gap: 0;
+        > div.d_flex {
+            flex-flow: column;
+            > span.mx_x0_5 {
+                display: none;
             }
         }
     }
-    @media (max-width: 580px), (max-height: 640px) {
-        svg.fill_icon\\.watchControllerBase {
-            padding-inline: 4px;
-        }
+}
+@media (max-width: 580px), (max-height: 640px) {
+    svg.fill_icon\\.watchControllerBase {
+        padding-inline: 4px;
     }
 }
 `);
@@ -99,120 +97,119 @@ const GM_addStyle = (content) => {
     let isBrowserFull = false;
 
     const browserFullStyleEl = GM_addStyle(`
-@media not all and (display-mode: fullscreen) {
-    :not(
-        :has([data-styling-id=":r2:"]),
-        [data-styling-id=":r2:"], [data-styling-id=":r2:"] *,
-        :has([data-nvpc-scope="watch-floating-panel"]),
-        [data-nvpc-scope="watch-floating-panel"],
-        [data-nvpc-scope="watch-floating-panel"] *,
-        :is([data-scope="toast"]), :is([data-scope="toast"]) *,
-        :is(body > [data-part="positioner"]), :is(body > [data-part="positioner"]) *,
-        :is(body > [data-part="backdrop"]), :is(body > [data-part="backdrop"]) *,
-        body
-    ) {
-        display: none;
-    }
-    body {
-        overflow-y: hidden !important;
-    }
-    #root > div.min-w_\\[max-content\\] {
-        min-width: auto;
-    }
-    div[aria-label="nicovideo-content"] {
-        padding-block: 0;
-    }
-    div.grid-area_\\[player\\] div.bdr_m {
-        border-radius: 0;
-    }
-    div[aria-label="nicovideo-content"] > section.grid-template-areas_\\[_\\"player_sidebar\\"_\\"meta_sidebar\\"_\\"bottom_sidebar\\"_\\"\\._sidebar\\"_\\] {
-        grid-template-columns: 100vw;
-        grid-template-areas: "player" "meta" "bottom" "sidebar";
-        padding-inline: 0;
-        padding-bottom: 0;
-        row-gap: 0;
-    }
-    div[data-styling-id=":r3:"] {
-        aspect-ratio: auto;
-        height: calc(
-            100vh
-            - var(--watch-controller-height)
-            - var(--watch-player-actionbar-gap-height)
-            - var(--watch-actionbar-height)
-        );
-    }
-    div.grid-area_\\[sidebar\\] > div[data-nvpc-part="floating"] > section {
-        position: fixed;
-        top: var(--spacing-x3);
-        width: var(--sizes-watch-sidebar-width);
-    }
-    div:has(> div > button[aria-label="コメント投稿ボタン"]) {
-        min-width: auto !important;
-    }
-    @media (max-width: 640px) {
-        div.d_flex:has(> button[aria-label$=" 秒戻る"]) {
-            gap: 0;
-            > div.d_flex {
-                flex-flow: column;
-                > span.mx_x0_5 {
-                    display: none;
-                }
+:not(
+    :has([data-styling-id=":r2:"]),
+    [data-styling-id=":r2:"], [data-styling-id=":r2:"] *,
+    :has([data-nvpc-scope="watch-floating-panel"]),
+    [data-nvpc-scope="watch-floating-panel"],
+    [data-nvpc-scope="watch-floating-panel"] *,
+    :is([data-scope="toast"]), :is([data-scope="toast"]) *,
+    :is(body > [data-part="positioner"]), :is(body > [data-part="positioner"]) *,
+    :is(body > [data-part="backdrop"]), :is(body > [data-part="backdrop"]) *,
+    body
+) {
+    display: none;
+}
+body {
+    overflow-y: hidden !important;
+}
+#root > div.min-w_\\[max-content\\] {
+    min-width: auto;
+}
+div[aria-label="nicovideo-content"] {
+    padding-block: 0;
+}
+div.grid-area_\\[player\\] div.bdr_m {
+    border-radius: 0;
+}
+div[aria-label="nicovideo-content"] > section.grid-template-areas_\\[_\\"player_sidebar\\"_\\"meta_sidebar\\"_\\"bottom_sidebar\\"_\\"\\._sidebar\\"_\\] {
+    grid-template-columns: 100vw;
+    grid-template-areas: "player" "meta" "bottom" "sidebar";
+    padding-inline: 0;
+    padding-bottom: 0;
+    row-gap: 0;
+}
+div[data-styling-id=":r3:"] {
+    aspect-ratio: auto;
+    height: calc(
+        100vh
+        - var(--watch-controller-height)
+        - var(--watch-player-actionbar-gap-height)
+        - var(--watch-actionbar-height)
+    );
+}
+div.grid-area_\\[sidebar\\] > div[data-nvpc-part="floating"] > section {
+    position: fixed;
+    top: var(--spacing-x3);
+    width: var(--sizes-watch-sidebar-width);
+}
+div:has(> div > button[aria-label="コメント投稿ボタン"]) {
+    min-width: auto !important;
+}
+@media (max-width: 640px) {
+    div.d_flex:has(> button[aria-label$=" 秒戻る"]) {
+        gap: 0;
+        > div.d_flex {
+            flex-flow: column;
+            > span.mx_x0_5 {
+                display: none;
             }
         }
     }
-    @media (max-width: 550px) {
-        div:has(> button[aria-label="設定"]) {
-            gap: 0;
-        }
+}
+@media (max-width: 550px) {
+    div:has(> button[aria-label="設定"]) {
+        gap: 0;
     }
-    #${THEATER_MODE_BUTTON_CONTAINER_ID}, button[aria-label="全画面表示する"] {
-        display: none;
-    }
+}
+#${THEATER_MODE_BUTTON_CONTAINER_ID}, button[aria-label="全画面表示する"] {
+    display: none;
 }
 `);
     browserFullStyleEl.disabled = true;
 
-    GM_addStyle(`
-@media not all and (display-mode: fullscreen) {
-    .npeb_container {
-        display: grid;
-        > .npeb_tooltip_container {
-            position: relative;
-            > .npeb_tooltip {
-                visibility: hidden;
-                min-width: max-content;
-                background-color: var(--colors-tooltip-background);
-                color: var(--colors-tooltip-text-on-background);
-                font-size: var(--font-sizes-s);
-                border-radius: var(--radii-s);
-                padding: var(--spacing-base);
-                position: absolute;
-                top: 0;
-                left: 50%;
-                transform: translate(-50%, -135%);
-                opacity: 0;
-                transition: opacity .2s .2s;
-            }
+    const baseStyleEl = GM_addStyle(`
+.npeb_container {
+    display: grid;
+    > .npeb_tooltip_container {
+        position: relative;
+        > .npeb_tooltip {
+            visibility: hidden;
+            min-width: max-content;
+            background-color: var(--colors-tooltip-background);
+            color: var(--colors-tooltip-text-on-background);
+            font-size: var(--font-sizes-s);
+            border-radius: var(--radii-s);
+            padding: var(--spacing-base);
+            position: absolute;
+            top: 0;
+            left: 50%;
+            transform: translate(-50%, -135%);
+            opacity: 0;
+            transition: opacity .2s .2s;
         }
     }
-    .npeb_container:is(:hover,[data-hover]) .npeb_tooltip {
-        visibility: visible;
-        opacity: 1;
-    }
-    .npeb_icon {
-        stroke: var(--colors-icon-watch-controller-base);
-    }
-    .npeb_icon:is(:hover,[data-hover]) {
-        stroke: var(--colors-icon-watch-controller-hover);
-    }
 }
-@media all and (display-mode: fullscreen) {
-    .npeb_container {
-        display: none;
-    }
+.npeb_container:is(:hover,[data-hover]) .npeb_tooltip {
+    visibility: visible;
+    opacity: 1;
+}
+.npeb_icon {
+    stroke: var(--colors-icon-watch-controller-base);
+}
+.npeb_icon:is(:hover,[data-hover]) {
+    stroke: var(--colors-icon-watch-controller-hover);
+}
+`);
+    baseStyleEl.disabled = true;
+
+    const videoFullscreenStyleEl = GM_addStyle(`
+.npeb_container {
+    display: none;
 }
 `);
 
+    let refreshButtonsAndStyles = () => { };
     let switchTheaterMode = () => { };
     let switchBrowserFull = () => { };
 
@@ -280,10 +277,13 @@ const GM_addStyle = (content) => {
             'ブラウザ内最大化解除（b）'
         );
 
-        const refreshButtonsAndStyles = async () => {
+        refreshButtonsAndStyles = async () => {
+            const isVideoFullscreen = document.fullscreenElement !== null;
+            baseStyleEl.disabled = isVideoFullscreen;
+            videoFullscreenStyleEl.disabled = !isVideoFullscreen;
             const isTheaterModeRequired = !isBrowserFull && getIsTheaterMode();
-            theaterModeStyleEl.disabled = !isTheaterModeRequired;
-            browserFullStyleEl.disabled = !isBrowserFull;
+            theaterModeStyleEl.disabled = isVideoFullscreen || !isTheaterModeRequired;
+            browserFullStyleEl.disabled = isVideoFullscreen || !isBrowserFull;
             setTheaterModeButtonIsOn(isTheaterModeRequired);
             setbrowserFullButtonIsOn(isBrowserFull);
         };
@@ -306,6 +306,10 @@ const GM_addStyle = (content) => {
 
         console.log('nicovideo-player-expander buttons are added.')
     };
+
+    document.addEventListener('fullscreenchange', () => {
+        refreshButtonsAndStyles();
+    });
 
     document.addEventListener('keydown', (e) => {
         if (document.fullscreenElement !== null) {
--- nicovideo-player-expander.user.js
+++ nicovideo-player-expander.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        nicovideo-player-expander
 // @namespace   https://github.com/dnek
-// @version     3.1
+// @version     3.2
 // @author      dnek
 // @description ニコニコ動画のプレイヤーを2種類の方法（「シアターモード」または「ブラウザ内最大化」）で拡大します。プレイヤー右下のアイコンでこれらの機能を切り替えられます。それぞれtキー、bキーでも（ブラウザ内最大化解除はescキーでも）切り替えられます。「nicovideo-next-video-canceler」「nicovideo-autoplay-canceler」は別のスクリプトです。
 // @description:ja    ニコニコ動画のプレイヤーを2種類の方法（「シアターモード」または「ブラウザ内最大化」）で拡大します。プレイヤー右下のアイコンでこれらの機能を切り替えられます。それぞれtキー、bキーでも（ブラウザ内最大化解除はescキーでも）切り替えられます。「nicovideo-next-video-canceler」「nicovideo-autoplay-canceler」は別のスクリプトです。
@@ -29,61 +29,59 @@
     };
 
     const theaterModeStyleEl = GM_addStyle(`
-@media not all and (display-mode: fullscreen) {
-    #root > div.min-w_\\[max-content\\] {
-        min-width: auto;
+#root > div.min-w_\\[max-content\\] {
+    min-width: auto;
+}
+div[aria-label="nicovideo-content"] > section.grid-template-areas_\\[_\\"player_sidebar\\"_\\"meta_sidebar\\"_\\"bottom_sidebar\\"_\\"\\._sidebar\\"_\\] {
+    --watch-player-expandable-width-by-browser: calc(
+        100vw
+        - var(--watch-layout-gap-width) * 2
+        - var(--scrollbar-width)
+    );
+    --watch-player-expandable-height-by-browser: calc(
+        100vh
+        - var(--common-header-height)
+        - var(--web-header-height)
+        - var(--watch-controller-height)
+        - var(--watch-actionbar-height)
+        - var(--watch-player-bottom-margin-height)
+    );
+    --watch-player-expandable-width: calc(
+        min(
+            var(--watch-player-expandable-width-by-browser),
+            var(--watch-player-expandable-height-by-browser) * (16 / 9)
+        )
+    );
+    grid-template-columns: var(--watch-player-expandable-width);
+    grid-template-areas: "player" "meta" "bottom" "sidebar";
+}
+div.grid-area_\\[sidebar\\] > div[data-nvpc-part="floating"] > section {
+    position: fixed;
+    top: calc(var(--sizes-common-header-in-view-height) + var(--sizes-web-header-height) + var(--spacing-x3));
+    width: var(--sizes-watch-sidebar-width);
+}
+div:has(> div > button[aria-label="コメント投稿ボタン"]) {
+    min-width: auto !important;
+}
+@media (max-width: 720px), (max-height: 720px) {
+    div:has(> button[aria-label="設定"]) {
+        gap: 0;
     }
-    div[aria-label="nicovideo-content"] > section.grid-template-areas_\\[_\\"player_sidebar\\"_\\"meta_sidebar\\"_\\"bottom_sidebar\\"_\\"\\._sidebar\\"_\\] {
-        --watch-player-expandable-width-by-browser: calc(
-            100vw
-            - var(--watch-layout-gap-width) * 2
-            - var(--scrollbar-width)
-        );
-        --watch-player-expandable-height-by-browser: calc(
-            100vh
-            - var(--common-header-height)
-            - var(--web-header-height)
-            - var(--watch-controller-height)
-            - var(--watch-actionbar-height)
-            - var(--watch-player-bottom-margin-height)
-        );
-        --watch-player-expandable-width: calc(
-            min(
-                var(--watch-player-expandable-width-by-browser),
-                var(--watch-player-expandable-height-by-browser) * (16 / 9)
-            )
-        );
-        grid-template-columns: var(--watch-player-expandable-width);
-        grid-template-areas: "player" "meta" "bottom" "sidebar";
-    }
-    div.grid-area_\\[sidebar\\] > div[data-nvpc-part="floating"] > section {
-        position: fixed;
-        top: calc(var(--sizes-common-header-in-view-height) + var(--sizes-web-header-height) + var(--spacing-x3));
-        width: var(--sizes-watch-sidebar-width);
-    }
-    div:has(> div > button[aria-label="コメント投稿ボタン"]) {
-        min-width: auto !important;
-    }
-    @media (max-width: 720px), (max-height: 720px) {
-        div:has(> button[aria-label="設定"]) {
-            gap: 0;
-        }
-    }
-    @media (max-width: 650px), (max-height: 680px) {
-        div.d_flex:has(> button[aria-label$=" 秒戻る"]) {
-            gap: 0;
-            > div.d_flex {
-                flex-flow: column;
-                > span.mx_x0_5 {
-                    display: none;
-                }
+}
+@media (max-width: 650px), (max-height: 680px) {
+    div.d_flex:has(> button[aria-label$=" 秒戻る"]) {
+        gap: 0;
+        > div.d_flex {
+            flex-flow: column;
+            > span.mx_x0_5 {
+                display: none;
             }
         }
     }
-    @media (max-width: 580px), (max-height: 640px) {
-        svg.fill_icon\\.watchControllerBase {
-            padding-inline: 4px;
-        }
+}
+@media (max-width: 580px), (max-height: 640px) {
+    svg.fill_icon\\.watchControllerBase {
+        padding-inline: 4px;
     }
 }
 `);
@@ -92,120 +90,119 @@
     let isBrowserFull = false;
 
     const browserFullStyleEl = GM_addStyle(`
-@media not all and (display-mode: fullscreen) {
-    :not(
-        :has([data-styling-id=":r2:"]),
-        [data-styling-id=":r2:"], [data-styling-id=":r2:"] *,
-        :has([data-nvpc-scope="watch-floating-panel"]),
-        [data-nvpc-scope="watch-floating-panel"],
-        [data-nvpc-scope="watch-floating-panel"] *,
-        :is([data-scope="toast"]), :is([data-scope="toast"]) *,
-        :is(body > [data-part="positioner"]), :is(body > [data-part="positioner"]) *,
-        :is(body > [data-part="backdrop"]), :is(body > [data-part="backdrop"]) *,
-        body
-    ) {
-        display: none;
-    }
-    body {
-        overflow-y: hidden !important;
-    }
-    #root > div.min-w_\\[max-content\\] {
-        min-width: auto;
-    }
-    div[aria-label="nicovideo-content"] {
-        padding-block: 0;
-    }
-    div.grid-area_\\[player\\] div.bdr_m {
-        border-radius: 0;
-    }
-    div[aria-label="nicovideo-content"] > section.grid-template-areas_\\[_\\"player_sidebar\\"_\\"meta_sidebar\\"_\\"bottom_sidebar\\"_\\"\\._sidebar\\"_\\] {
-        grid-template-columns: 100vw;
-        grid-template-areas: "player" "meta" "bottom" "sidebar";
-        padding-inline: 0;
-        padding-bottom: 0;
-        row-gap: 0;
-    }
-    div[data-styling-id=":r3:"] {
-        aspect-ratio: auto;
-        height: calc(
-            100vh
-            - var(--watch-controller-height)
-            - var(--watch-player-actionbar-gap-height)
-            - var(--watch-actionbar-height)
-        );
-    }
-    div.grid-area_\\[sidebar\\] > div[data-nvpc-part="floating"] > section {
-        position: fixed;
-        top: var(--spacing-x3);
-        width: var(--sizes-watch-sidebar-width);
-    }
-    div:has(> div > button[aria-label="コメント投稿ボタン"]) {
-        min-width: auto !important;
-    }
-    @media (max-width: 640px) {
-        div.d_flex:has(> button[aria-label$=" 秒戻る"]) {
-            gap: 0;
-            > div.d_flex {
-                flex-flow: column;
-                > span.mx_x0_5 {
-                    display: none;
-                }
+:not(
+    :has([data-styling-id=":r2:"]),
+    [data-styling-id=":r2:"], [data-styling-id=":r2:"] *,
+    :has([data-nvpc-scope="watch-floating-panel"]),
+    [data-nvpc-scope="watch-floating-panel"],
+    [data-nvpc-scope="watch-floating-panel"] *,
+    :is([data-scope="toast"]), :is([data-scope="toast"]) *,
+    :is(body > [data-part="positioner"]), :is(body > [data-part="positioner"]) *,
+    :is(body > [data-part="backdrop"]), :is(body > [data-part="backdrop"]) *,
+    body
+) {
+    display: none;
+}
+body {
+    overflow-y: hidden !important;
+}
+#root > div.min-w_\\[max-content\\] {
+    min-width: auto;
+}
+div[aria-label="nicovideo-content"] {
+    padding-block: 0;
+}
+div.grid-area_\\[player\\] div.bdr_m {
+    border-radius: 0;
+}
+div[aria-label="nicovideo-content"] > section.grid-template-areas_\\[_\\"player_sidebar\\"_\\"meta_sidebar\\"_\\"bottom_sidebar\\"_\\"\\._sidebar\\"_\\] {
+    grid-template-columns: 100vw;
+    grid-template-areas: "player" "meta" "bottom" "sidebar";
+    padding-inline: 0;
+    padding-bottom: 0;
+    row-gap: 0;
+}
+div[data-styling-id=":r3:"] {
+    aspect-ratio: auto;
+    height: calc(
+        100vh
+        - var(--watch-controller-height)
+        - var(--watch-player-actionbar-gap-height)
+        - var(--watch-actionbar-height)
+    );
+}
+div.grid-area_\\[sidebar\\] > div[data-nvpc-part="floating"] > section {
+    position: fixed;
+    top: var(--spacing-x3);
+    width: var(--sizes-watch-sidebar-width);
+}
+div:has(> div > button[aria-label="コメント投稿ボタン"]) {
+    min-width: auto !important;
+}
+@media (max-width: 640px) {
+    div.d_flex:has(> button[aria-label$=" 秒戻る"]) {
+        gap: 0;
+        > div.d_flex {
+            flex-flow: column;
+            > span.mx_x0_5 {
+                display: none;
             }
         }
     }
-    @media (max-width: 550px) {
-        div:has(> button[aria-label="設定"]) {
-            gap: 0;
-        }
+}
+@media (max-width: 550px) {
+    div:has(> button[aria-label="設定"]) {
+        gap: 0;
     }
-    #${THEATER_MODE_BUTTON_CONTAINER_ID}, button[aria-label="全画面表示する"] {
-        display: none;
-    }
+}
+#${THEATER_MODE_BUTTON_CONTAINER_ID}, button[aria-label="全画面表示する"] {
+    display: none;
 }
 `);
     browserFullStyleEl.disabled = true;
 
-    GM_addStyle(`
-@media not all and (display-mode: fullscreen) {
-    .npeb_container {
-        display: grid;
-        > .npeb_tooltip_container {
-            position: relative;
-            > .npeb_tooltip {
-                visibility: hidden;
-                min-width: max-content;
-                background-color: var(--colors-tooltip-background);
-                color: var(--colors-tooltip-text-on-background);
-                font-size: var(--font-sizes-s);
-                border-radius: var(--radii-s);
-                padding: var(--spacing-base);
-                position: absolute;
-                top: 0;
-                left: 50%;
-                transform: translate(-50%, -135%);
-                opacity: 0;
-                transition: opacity .2s .2s;
-            }
+    const baseStyleEl = GM_addStyle(`
+.npeb_container {
+    display: grid;
+    > .npeb_tooltip_container {
+        position: relative;
+        > .npeb_tooltip {
+            visibility: hidden;
+            min-width: max-content;
+            background-color: var(--colors-tooltip-background);
+            color: var(--colors-tooltip-text-on-background);
+            font-size: var(--font-sizes-s);
+            border-radius: var(--radii-s);
+            padding: var(--spacing-base);
+            position: absolute;
+            top: 0;
+            left: 50%;
+            transform: translate(-50%, -135%);
+            opacity: 0;
+            transition: opacity .2s .2s;
         }
     }
-    .npeb_container:is(:hover,[data-hover]) .npeb_tooltip {
-        visibility: visible;
-        opacity: 1;
-    }
-    .npeb_icon {
-        stroke: var(--colors-icon-watch-controller-base);
-    }
-    .npeb_icon:is(:hover,[data-hover]) {
-        stroke: var(--colors-icon-watch-controller-hover);
-    }
 }
-@media all and (display-mode: fullscreen) {
-    .npeb_container {
-        display: none;
-    }
+.npeb_container:is(:hover,[data-hover]) .npeb_tooltip {
+    visibility: visible;
+    opacity: 1;
+}
+.npeb_icon {
+    stroke: var(--colors-icon-watch-controller-base);
+}
+.npeb_icon:is(:hover,[data-hover]) {
+    stroke: var(--colors-icon-watch-controller-hover);
+}
+`);
+    baseStyleEl.disabled = true;
+
+    const videoFullscreenStyleEl = GM_addStyle(`
+.npeb_container {
+    display: none;
 }
 `);
 
+    let refreshButtonsAndStyles = () => { };
     let switchTheaterMode = () => { };
     let switchBrowserFull = () => { };
 
@@ -273,10 +270,13 @@
             'ブラウザ内最大化解除（b）'
         );
 
-        const refreshButtonsAndStyles = async () => {
+        refreshButtonsAndStyles = async () => {
+            const isVideoFullscreen = document.fullscreenElement !== null;
+            baseStyleEl.disabled = isVideoFullscreen;
+            videoFullscreenStyleEl.disabled = !isVideoFullscreen;
             const isTheaterModeRequired = !isBrowserFull && getIsTheaterMode();
-            theaterModeStyleEl.disabled = !isTheaterModeRequired;
-            browserFullStyleEl.disabled = !isBrowserFull;
+            theaterModeStyleEl.disabled = isVideoFullscreen || !isTheaterModeRequired;
+            browserFullStyleEl.disabled = isVideoFullscreen || !isBrowserFull;
             setTheaterModeButtonIsOn(isTheaterModeRequired);
             setbrowserFullButtonIsOn(isBrowserFull);
         };
@@ -299,6 +299,10 @@
 
         console.log('nicovideo-player-expander buttons are added.')
     };
+
+    document.addEventListener('fullscreenchange', () => {
+        refreshButtonsAndStyles();
+    });
 
     document.addEventListener('keydown', (e) => {
         if (document.fullscreenElement !== null) {
